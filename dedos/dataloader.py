67ca6168992fd395b96f97e69b33ab827379fb08
Edwin <edwinpan@stanford.edu>
2021-11-10 22:44:07 -0800

    Moving to PIL for image loading for transform consistency

--- dedos/dataloader.py
+++ dedos/dataloader.py
@@ -4,14 +4,17 @@ from torchvision import transforms
 from torch.utils.data import Subset
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+from PIL import Image
 
 import matplotlib.pyplot as plt
 import os.path as osp
 import os
-import cv2
 import glob
+import sys
 
-from metrics import Metrics
+sys.path.append('../') # importing in unit tests
+from dedos.metrics import Metrics
 
 def train_val_test_dataset(dataset, val_split=0.125, test_split=0.1):
     train_idx, test_idx = train_test_split(list(range(len(dataset))), test_size=test_split, random_state=1)
@@ -67,8 +70,8 @@ class DeDOSDataset(Dataset):
         encoded = self.e_images[index] # blurry now has path to chosen blurry image
         sharp = self.s_images[index] # sharp now has path to chosen sharp image
         
-        encoded = cv2.cvtColor(cv2.imread(encoded), cv2.COLOR_BGR2RGB)
-        sharp = cv2.cvtColor(cv2.imread(sharp), cv2.COLOR_BGR2RGB)
+        encoded = Image.open(encoded)
+        sharp = Image.open(sharp)
         
         encoded = self.preprocess(encoded)
         sharp = self.preprocess(sharp)
@@ -82,22 +85,27 @@ class DeDOSDataset(Dataset):
 if __name__ == '__main__':
     batchsize=2
     num_workers=4
-    dataset = DeDOSDataset('../../../data/deblurGAN/')
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    print(f"Using: {device}")
+
+    preprocess = transforms.Compose([transforms.Resize(size=(256,256), interpolation=transforms.InterpolationMode.NEAREST), # Using bilinear interpolation
+                                     transforms.ToTensor()])
+
+    dataset = DeDOSDataset('../../../data/deblurGAN/', preprocess=preprocess)
     datasets = train_val_test_dataset(dataset)
     dataloaders = {x:DataLoader(datasets[x],batchsize, shuffle=True, num_workers=num_workers) for x in ['train','val','test']}
-
     dl = dataloaders['train']
+    metrics = Metrics(device=device)
 
-    metrics = Metrics(device='cpu')
-
-    sample = next(iter(dl))
-
-    out = metrics(sample[1],sample[0])
+    blurry, sharp = next(iter(dl))
+    blurry = blurry.to(device)
+    sharp = sharp.to(device)
+    out = metrics(blurry,sharp)
 
     print(out)
-    print(sample[0].shape)
-    plt.imshow(sample[0][0].permute(1,2,0))
+    print(blurry.shape)
+    plt.imshow(blurry[0].cpu().permute(1,2,0))
     plt.savefig('./blur.png')
-    plt.imshow(sample[1][0].permute(1,2,0))
+    plt.imshow(sharp[0].cpu().permute(1,2,0))
     plt.savefig('./sharp.png')
 
