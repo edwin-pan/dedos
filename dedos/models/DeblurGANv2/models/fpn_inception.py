031bafcd8702a2d3e26f84f53598f40e73767ff5
Anthony <anthony.vento@utexas.edu>
2021-11-24 23:24:12 -0800

    training

--- dedos/models/DeblurGANv2/models/fpn_inception.py
+++ dedos/models/DeblurGANv2/models/fpn_inception.py
@@ -65,10 +65,10 @@ class FPNInception(nn.Module):
         self.noise_weight2 = torch.nn.Parameter(torch.randn(1,num_filters,1,1),requires_grad=True)
         self.noise_weight1 = torch.nn.Parameter(torch.randn(1,num_filters,1,1),requires_grad=True)
 
-        self.noise_val4 = torch.nn.Parameter(torch.randn((1, 1, 64, 64)),requires_grad=True)
-        self.noise_val3 = torch.nn.Parameter(torch.randn((1, 1, 32, 32)),requires_grad=True)
-        self.noise_val2 = torch.nn.Parameter(torch.randn((1, 1, 16, 16)),requires_grad=True)
-        self.noise_val1 = torch.nn.Parameter(torch.randn((1, 1, 8, 8)),requires_grad=True)
+        #self.noise_val4 = torch.nn.Parameter(torch.randn((1, 1, 64, 64)),requires_grad=True)
+        #self.noise_val3 = torch.nn.Parameter(torch.randn((1, 1, 32, 32)),requires_grad=True)
+        #self.noise_val2 = torch.nn.Parameter(torch.randn((1, 1, 16, 16)),requires_grad=True)
+        #self.noise_val1 = torch.nn.Parameter(torch.randn((1, 1, 8, 8)),requires_grad=True)
 
         self.final = nn.Conv2d(num_filters // 2, output_ch, kernel_size=3, padding=1)
 
@@ -84,10 +84,10 @@ class FPNInception(nn.Module):
 
         if self.add_noise:
             if not self.optimize_noise:
-                self.noise_val4.data = torch.randn((map4.shape[0], 1, map4.shape[2],map4.shape[3])).cuda()
-                self.noise_val3.data = torch.randn((map3.shape[0], 1, map3.shape[2],map3.shape[3])).cuda()
-                self.noise_val2.data = torch.randn((map2.shape[0], 1, map2.shape[2],map2.shape[3])).cuda()
-                self.noise_val1.data = torch.randn((map1.shape[0], 1, map1.shape[2],map1.shape[3])).cuda()
+                self.noise_val4 = torch.randn((map4.shape[0], 1, map4.shape[2],map4.shape[3])).cuda()
+                self.noise_val3 = torch.randn((map3.shape[0], 1, map3.shape[2],map3.shape[3])).cuda()
+                self.noise_val2 = torch.randn((map2.shape[0], 1, map2.shape[2],map2.shape[3])).cuda()
+                self.noise_val1 = torch.randn((map1.shape[0], 1, map1.shape[2],map1.shape[3])).cuda()
                 
             map4 += self.noise_val4 * self.noise_weight4
             map3 += self.noise_val3 * self.noise_weight3
